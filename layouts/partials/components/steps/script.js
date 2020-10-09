913cd6acbad21e9b7c835788f66a7ccc34fcd3bf
Bertrand Keller <bertrand.keller@gmail.com>
2020-10-09 16:41:02 +0200

    first commit

---
+++ layouts/partials/components/steps/script.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="/js/van11y-accessible-tab-panel-aria.min.js" defer></script>
---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './public/**/*.html',
+    './layouts/*.html',
+    './layouts/**/*.html',
+    './theme/overgo/layouts/*.html',
+    './theme/overgo/layouts/**/*.html'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: ['>1%']
+    }),
+    require('stylelint'),
+    require("postcss-unused-var"),
+    require("css-mqpacker"),
+    require('cssnano')({
+      preset: 'default',
+    }),
+    purgecss
+  ]
+}
